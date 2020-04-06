f7dc0261eef5a71a18e56f0cc2fc43d8c8d27c5b
Szymon <s.bukowski07@gmail.com>
2020-04-06 09:16:26 +0200

    server main

--- SERVER-JavaRMI/src/MyServer.java
+++ SERVER-JavaRMI/src/MyServer.java
@@ -1,5 +1,8 @@
 import old.CalcObjectImplementation;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class MyServer {
 
     public static void main(String[] args)
@@ -9,12 +12,24 @@ public class MyServer {
             return;
         }
 
-        if (System.getSecurityManager() == null)
-            System.setSecurityManager(new SecurityManager());
+        String adres1 = args[0];
+        String adres2 = args[1];
+        int port = 1099;
 
         try {
-            CalcObjectImplementation implObiektu = new CalcObjectImplementation();
-            java.rmi.Naming.rebind(args[0], implObiektu);
+            Registry reg = LocateRegistry.createRegistry((port));
+        }catch(Exception e){
+
+        }
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+
+        try {
+            Worker worker = new WorkerImplementation();
+            java.rmi.Naming.rebind(adres1, worker );
+            Worker worker2 = new WorkerImplementation();
+            java.rmi.Naming.rebind(adres2, worker2 );
             System.out.println("Server is registered now :-)");
             System.out.println("Press Crl+C to stop...");
         } catch (Exception e) {
